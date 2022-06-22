11ea198c3ac03d6adc212f1bf4f5ae5946e47506
NirajanJoc <Nirajanjoc19@gmail.com>
2022-06-22 09:35:23 +0545

    'user_based_authentication'

--- user/decorators.py
+++ user/decorators.py
@@ -23,3 +23,29 @@ def allowed_users(allowed_roles=[]):
                 return HttpResponse("You are not superuser. You can't login to main dashboard")
         return wrapper_func
     return decorator
+
+def admin_only(view_func):
+    def wrapper_function(request, *args, **kwargs):
+        group = None
+        if request.user.groups.exists():
+            group = request.user.groups.all()[0].name
+        
+        if group == 'customer':
+            return HttpResponseRedirect(reverse('user:user_view'))
+        if group == 'admin':
+            return view_func(request, *args, **kwargs)
+    return wrapper_function
+
+# def admin_only(view_func):
+# 	def wrapper_function(request, *args, **kwargs):
+# 		group = None
+# 		if request.user.groups.exists():
+# 			group = request.user.groups.all()[0].name
+
+# 		if group == 'customer':
+# 			return HttpResponseRedirect(reverse('user:user_view'))
+
+# 		if group == 'admin':
+# 			return view_func(request, *args, **kwargs)
+
+# 	return wrapper_function
--- user/views.py
+++ user/views.py
@@ -1,5 +1,3 @@
-from multiprocessing import context
-from re import A
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, reverse
 from customer.models import Customer
@@ -9,11 +7,12 @@ from django.contrib.auth.forms import AuthenticationForm
 from user.forms import CreateUserForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from user.decorators import unauthenticated, allowed_users
+from user.decorators import unauthenticated, admin_only, allowed_users
+from django.contrib.auth.models import Group
 # Create your views here.
-
+# @allowed_users(allowed_roles=['admin'])
 @login_required(login_url='user:login')
-@allowed_users(allowed_roles=['admin'])
+# @admin_only
 def dashboard(request):
     customers=Customer.objects.all()
     orders=Order.objects.all()
@@ -39,7 +38,10 @@ def login_view(request):
         user=authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return HttpResponseRedirect(reverse('user:dashboard'))
+            if user.is_superuser:
+                return HttpResponseRedirect(reverse('user:dashboard'))
+            else:
+                return HttpResponseRedirect(reverse('user:user_view'))
         else:
             messages.info(request, 'Username or Password is Incorrect')
     context={'form':form}
@@ -51,9 +53,12 @@ def register_view(request):
     form=CreateUserForm(request.POST or None)
     if request.POST:
         if form.is_valid():
-            form.save()
-            user=form.cleaned_data.get('username')
-            messages.success(request, "Account has been created for "+user)
+            user=form.save()
+            username=form.cleaned_data.get('username')
+            group=Group.objects.get(name='customer')
+            user.groups.add(group)
+            Customer.objects.create(user=user)
+            messages.success(request, "Account has been created for "+username)
             return HttpResponseRedirect(reverse('user:login'))
     context={'form':form}
     return render(request, 'register.html', context)
@@ -62,6 +67,18 @@ def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse('user:login'))
 
+@login_required(login_url='user:login')
+@allowed_users(allowed_roles=['customer'])
 def user_view(request):
-    context={}
+    orders=request.user.customer.order_set.all()
+    total_order=orders.count()
+    delivered=orders.filter(status='Delivery').count()
+    pending=orders.filter(status='Pending').count()
+
+    print("Orders", orders)
+    context={"orders":orders,
+    'total_order':total_order,
+    'delivered':delivered,
+    'pending':pending,
+    }
     return render(request,'user.html', context)
